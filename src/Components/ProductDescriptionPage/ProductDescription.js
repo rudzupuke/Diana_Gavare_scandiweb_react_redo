a42e8e03155e61a3b5050fef357709438a8b3434
rudzupuke <diana.gavare2@gmail.com>
2022-06-01 09:26:11 +0300

    sanitize string before parsing as HTML

--- src/Components/ProductDescriptionPage/ProductDescription.js
+++ src/Components/ProductDescriptionPage/ProductDescription.js
@@ -1,12 +1,14 @@
 import { Component } from "react";
+import dompurify from "dompurify";
 
 class ProductDescription extends Component {
+    sanitize = dompurify.sanitize;
     render() {
         return (
             <div
                 className="product-description-container"
                 dangerouslySetInnerHTML={{
-                    __html: this.props.productDescription,
+                    __html: this.sanitize(this.props.productDescription),
                 }}
             ></div>
         );
